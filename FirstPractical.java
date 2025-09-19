24576b50ae79b632c5d1b4b0170ff89a23374baf
akshat1456 <akshatkorde@gmail.com>
2025-09-19 14:17:49 +0530

    Update FirstPractical.java
    final one

--- FirstPractical.java
+++ FirstPractical.java
@@ -1,7 +1,8 @@
 
 public class FirstPractical {
     public static void main(String[] args) {
-        System.out.println("This is my first practical for Jenkins
+        System.out.println("This is my first practical for Jenkins");
     }
 }
+
 
