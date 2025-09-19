570327b481d38f5281a7bd5c05ca6d904b64f5fa
akshat1456 <akshatkorde@gmail.com>
2025-09-19 14:08:36 +0530

    Update FirstPractical.java
    new update without bracket

--- FirstPractical.java
+++ FirstPractical.java
@@ -1,6 +1,7 @@
 
 public class FirstPractical {
     public static void main(String[] args) {
-        System.out.println("This is my first practical for Jenkins");
+        System.out.println("This is my first practical for Jenkins
     }
 }
+
